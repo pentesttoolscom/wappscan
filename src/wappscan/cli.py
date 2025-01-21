f940c007a433df76dc1a40cc83bbd3cfdf8d6630
cosmin-paunica-ptt <cosmin.paunica@pentest-tools.com>
2025-01-21 13:42:45 +0200

    feat: print json instead of dict

--- src/wappscan/cli.py
+++ src/wappscan/cli.py
@@ -1,9 +1,10 @@
 """Functions for CLI interaction"""
 
+import json
 from uuid import UUID
 
 import typer
-from rich import print as rich_print
+from rich import print_json
 
 from wappscan.core import get_scan_info
 
@@ -13,7 +14,8 @@ app = typer.Typer(no_args_is_help=True)
 @app.command()
 def get(scan_id: UUID):
     """Get scan by ID"""
-    rich_print(get_scan_info(scan_id))
+    scan_result = get_scan_info(scan_id)
+    print_json(json.dumps(scan_result))
 
 
 @app.command()
